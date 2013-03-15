e44c21a8ba98e5ba47b1d1eaba413b19f33e0f7e
Ben Burry <bburry@makielab.com>
2013-03-15 11:44:31 +0000

    Change version to 1.5.2

--- seacucumber/__init__.py
+++ seacucumber/__init__.py
@@ -3,5 +3,5 @@ Sea Cucumber is a Django email backend for Amazon Simple Email Service,
 backed by celery. The interesting bits are in backend.py and tasks.py. The
 rest of the contents of this module are largely optional. 
 """
-# In the form of Major, Minor.
-VERSION = (1, 5, 1)
+# In the form of Major, Minor, Revision.
+VERSION = (1, 5, 2)
