f5fcb234cd9ebd655e00405f323dc0516be9325c
Ben Burry <bburry@makielab.com>
2013-03-15 12:36:17 +0000

    Don't be insane - just string match (and assume boto aren't going to version up to 2.50)

--- seacucumber/__init__.py
+++ seacucumber/__init__.py
@@ -4,16 +4,7 @@ backed by celery. The interesting bits are in backend.py and tasks.py. The
 rest of the contents of this module are largely optional. 
 """
 from boto import Version as BotoVersion
-import re
 
 # In the form of Major, Minor, Revision.
 VERSION = (1, 5, 2)
-FORCE_UNICODE = False
-
-m = re.search(r'^\d+\.\d+', BotoVersion)
-if m is not None:
-    try:
-        if float(m.group(0)) == 2.5:
-            FORCE_UNICODE = True
-    except:
-        pass
+FORCE_UNICODE = BotoVersion[:3] == '2.5'
