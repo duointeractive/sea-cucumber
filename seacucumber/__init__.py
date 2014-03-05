7e284ee41d69bcacbc40d4231cbeca902c930199
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2014-03-05 15:13:54 +0000

    Be more robust about picking boto version 2.5.x.

--- seacucumber/__init__.py
+++ seacucumber/__init__.py
@@ -1,10 +1,11 @@
 """
-Sea Cucumber is a Django email backend for Amazon Simple Email Service, 
+Sea Cucumber is a Django email backend for Amazon Simple Email Service,
 backed by celery. The interesting bits are in backend.py and tasks.py. The
-rest of the contents of this module are largely optional. 
+rest of the contents of this module are largely optional.
 """
 from boto import Version as BotoVersion
 
 # In the form of Major, Minor, Revision.
 VERSION = (1, 5, 2)
-FORCE_UNICODE = BotoVersion[:3] == '2.5'
+
+FORCE_UNICODE = BotoVersion[:4] == '2.5.'
